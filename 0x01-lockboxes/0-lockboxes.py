4c93a1313f5406e36b02bdd035ab9c04b8c5732c
bramans <ibrahim12mansour20@gmail.com>
2024-10-09 09:22:13 -0700

    Optimize_code

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,27 +1,21 @@
 #!/usr/bin/python3
-"""
-Optimized solution to the lockboxes problem
-"""
+'''A module for working with lockboxes.
+'''
 
+def canOpenAll(lockboxes):
+    '''Checks if all the lockboxes in a list of boxes containing keys
+    (indices) to other lockboxes can be unlocked, starting with the
+    first one unlocked.
+    '''
+    total_boxes = len(lockboxes)
+    unlocked_boxes = set([0])  # Track which boxes are unlocked
+    available_keys = set(lockboxes[0]).difference(set([0]))  # Keys from the first box
 
-def canUnlockAll(boxes):
-    """
-    Determines whether all boxes can be unlocked.
-    Each box contains keys to other boxes.
-    """
-    if not isinstance(boxes, list) or len(boxes) == 0:
-        return False
-
-    n = len(boxes)
-    seen_boxes = {0}  # Set of unlocked boxes, starting with the first box
-    unseen_boxes = set(boxes[0])  # Keys available from the first box
-
-    while unseen_boxes:
-        boxIdx = unseen_boxes.pop()
-        if boxIdx < 0 or boxIdx >= n or boxIdx in seen_boxes:
-            continue  # Skip invalid or already seen boxes
-        seen_boxes.add(boxIdx)
-        unseen_boxes.update(boxes[boxIdx])  # Add new keys from the current box
-
-    return len(seen_boxes) == n
-
+    while available_keys:
+        current_key = available_keys.pop()
+        if current_key < 0 or current_key >= total_boxes or current_key in unlocked_boxes:
+            continue  # Skip invalid or already unlocked boxes
+        unlocked_boxes.add(current_key)
+        available_keys.update(lockboxes[current_key])  # Add new keys from the current box
+    
+    return len(unlocked_boxes) == total_boxes  # Return True if all boxes are unlocked
