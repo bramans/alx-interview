c57cbd6d01326c7f7a8e59cb703ed24fd6e69a2a
bramans <ibrahim12mansour20@gmail.com>
2024-10-09 09:24:17 -0700

    Optimize_code

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,21 +1,27 @@
 #!/usr/bin/python3
-'''A module for working with lockboxes.
-'''
+"""
+Solution for determining if all lockboxes can be unlocked.
+"""
 
-def canOpenAll(lockboxes):
-    '''Checks if all the lockboxes in a list of boxes containing keys
-    (indices) to other lockboxes can be unlocked, starting with the
-    first one unlocked.
-    '''
-    total_boxes = len(lockboxes)
-    unlocked_boxes = set([0])  # Track which boxes are unlocked
-    available_keys = set(lockboxes[0]).difference(set([0]))  # Keys from the first box
 
-    while available_keys:
-        current_key = available_keys.pop()
-        if current_key < 0 or current_key >= total_boxes or current_key in unlocked_boxes:
-            continue  # Skip invalid or already unlocked boxes
-        unlocked_boxes.add(current_key)
-        available_keys.update(lockboxes[current_key])  # Add new keys from the current box
-    
-    return len(unlocked_boxes) == total_boxes  # Return True if all boxes are unlocked
+def canUnlockAll(boxes):
+    """
+    Determines whether all lockboxes can be unlocked given a list where each
+    box contains keys to other boxes.
+    - The first box is always unlocked.
+    - A box can only be unlocked if you have its corresponding key.
+    """
+    if not isinstance(boxes, list):
+        return False
+    elif len(boxes) == 0:
+        return False
+
+    for key in range(1, len(boxes) - 1):
+        box_is_unlocked = False
+        for idx in range(len(boxes)):
+            box_is_unlocked = key in boxes[idx] and key != idx
+            if box_is_unlocked:
+                break
+        if not box_is_unlocked:
+            return box_is_unlocked
+    return True
