cb9a019794a897cdab48c233ba396bf2a74816d5
bramans <ibrahim12mansour20@gmail.com>
2024-10-16 08:38:35 -0700

    MinimumOperation

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,28 +1,25 @@
 #!/usr/bin/python3
-""" Calculate Minimum Operations """
+"""Minimum Operations"""
 
 def minOperations(n):
-    """
-    Calculates the minimum number of operations needed to result in exactly
-    n 'H' characters in the file, based on prime factorization logic without 
-    separating the steps.
-    
-    Returns:
-        The minimum number of operations as an integer.
-        Returns 0 if n is less than or equal to 1.
-    """
-    if n < 2:
+	"""Calculates the fewest number of operations needed
+        to result in exactly n H characters in the file"""
+    if n <= 1:
         return 0
-
-    total_operations = 0
-    divisor = 2
-
-    while divisor <= n:
-        # If divisor is a factor of n, reduce n by dividing and accumulate operations
-        while n % divisor == 0:
-            total_operations += divisor
-            n //= divisor
-        divisor += 1
-
-    return total_operations
-
+    
+    operations = 0
+    factor = 2
+    
+    while n > 1:
+        while n % factor == 0:
+            operations += factor
+            n //= factor
+        factor += 1
+        
+        # Optimization: If factor > sqrt(n), n must be prime
+        if factor * factor > n:
+            if n > 1:
+                operations += n
+            break
+    
+    return operations
